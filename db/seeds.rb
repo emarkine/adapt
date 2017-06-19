7c1438936cf1743c4b171ca1db9856f5ee4565eb
Eugene Markine <emarkine@gmail.com>
2017-06-19 19:18:43 +0200

    2017-06-19.19:18

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(name: 'Test', surname: 'User',
+user = User.create({ name: 'Test', surname: 'User',
                    mobile: '0612345678', email: 'test@marketram.com',
-                   password: 'password', password_confirmation: 'password' )
+                   password: 'password', password_confirmation: 'password' } )
 user.save!
