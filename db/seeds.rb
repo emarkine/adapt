645d601088a2cadede2a27d1e6b2e9908455ae2a
Eugene Markine <emarkine@gmail.com>
2017-06-30 20:48:33 +0200

    user

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,25 +3,27 @@ require 'rake'
 # Rake::Task['db:model:load'].invoke('country')
 # Rake::Task['db:model:load'].invoke('currency')
 
+Country.destroy_all
 US = Country.create( name: 'United States', code: 'US' )
 US.save!
-USD = Currency.create( name: 'United States Dollar', code: 'USD', sign: '$', country: US, state: 'market' )
-USD.save!
 NL = Country.create( name: 'Netherlands', code: 'NL' )
 NL.save!
-EUR = Currency.create( name: 'Euro', code: 'EUR', sign: '€', country: NL, state: 'market' )
+
+Currency.destroy_all
+USD = Currency.create( name: 'United States Dollar', code: 'USD', sign: '$', country: US)
+USD.save!
+EUR = Currency.create( name: 'Euro', code: 'EUR', sign: '€', country: NL )
 EUR.save!
 
+User.destroy_all
 USER_TEST = User.create({ name: 'Test', surname: 'User', currency: EUR,
                    mobile: '0612345678', email: 'test@marketram.com',
                    password: 'password', password_confirmation: 'password' } )
 USER_TEST.save!
-
 USER_ADMIN = User.create({ name: 'Admin', surname: 'User', currency: EUR,
                            mobile: '0612345678', email: 'admin@marketram.com',
                            password: '123456', password_confirmation: '123456' } )
 USER_ADMIN.save!
-
 USER_EUGENE = User.create({ name: 'Eugene', surname: 'Markine', currency: USD,
                      mobile: '0628736786', email: 'eugene@markine.nl',
                      password: 'secret123', password_confirmation: 'secret123' } )
