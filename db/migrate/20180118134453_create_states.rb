e68230030e80bc37b23ca52a7a26824f8eb55764
Eugene <emarkine@gmail.com>
2018-01-20 17:25:15 +0100

    2018-01-20.17:25

--- db/migrate/20180118134453_create_states.rb
+++ db/migrate/20180118134453_create_states.rb
@@ -1,10 +1,12 @@
 class CreateStates < ActiveRecord::Migration[5.1]
   def change
     create_table :states do |t|
-      t.string :name
-      t.reference :user
-      t.reference :fund
+      # t.string :name
+      t.reference :host
+      # t.reference :user
+      # t.reference :fund
       t.reference :service
+      t.string status
       t.timestamps
     end
   end
