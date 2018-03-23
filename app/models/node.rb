4b8c64a5fcb94ec07b9292cfc10624a8a3d16058
Eugene <emarkine@gmail.com>
2018-03-23 13:49:31 +0100

    rspec

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,11 +1,11 @@
 class Node < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :name, presence: true
-  # has_many :structures
-  # has_many :edges, through: :structures
-  # has_many :crystals, through: :structures
-  # has_many :neurons, through: :edges
-  # has_many :nerves, class_name: :Nerve, foreign_key: :node_id
+  has_many :structures
+  has_many :edges, through: :structures
+  has_many :crystals, through: :structures
+  has_many :neurons, through: :edges
+  has_many :nerves, class_name: :Nerve, foreign_key: :node_id
 
   # belongs_to :parent, class_name: :Node, foreign_key: :parent_id
   # belongs_to :next, class_name: :Node, foreign_key: :next_id
--- spec/models/node_spec.rb
+++ spec/models/node_spec.rb
@@ -2,22 +2,55 @@ require 'rails_helper'
 
 RSpec.describe Node, type: :model do
     fixtures :nodes
+    fixtures :structures
+    fixtures :edges
+    fixtures :neurons
+    fixtures :crystals
+
     let(:node)  { Node.find_by_name 'test' }
 
     it 'has a valid test'  do
       expect(node).to be_valid
     end
 
-    # it 'has a valid build factory'  do
-    #   expect(build(:node)).to be_valid
-    # end
-    #
-    # it 'is invalid without a name' do
-    #   expect(build(:node, name: nil)).not_to be_valid
-    # end
-    #
-    # it 'is invalid without a title' do
-    #   expect(build(:node, title: nil)).not_to be_valid
-    # end
+    it 'is invalid without a name' do
+      node.name = nil
+      expect(node).not_to be_valid
+    end
+
+    it 'is invalid without a title' do
+      node.title = nil
+      expect(node).not_to be_valid
+    end
+
+    it 'has not empty list of strictures'  do
+      expect(node.structures.empty?).to be false
+      expect(node.structures.first).to be_instance_of Structure
+    end
+
+    it 'has not empty list of strictures'  do
+      expect(node.structures.empty?).to be false
+      expect(node.structures.first).to be_instance_of Structure
+    end
+
+    it 'has not empty list of edges'  do
+      expect(node.edges.empty?).to be false
+      expect(node.edges.first).to be_instance_of Edge
+    end
+
+    it 'has not empty list of crystals'  do
+      expect(node.crystals.empty?).to be false
+      expect(node.crystals.first).to be_instance_of Crystal
+    end
+
+    it 'has not empty list of neurons'  do
+      expect(node.neurons.empty?).to be false
+      expect(node.neurons.first).to be_instance_of Neuron::Out
+    end
+
+    it 'has not empty list of nerves'  do
+      expect(node.nerves.empty?).to be false
+      expect(node.nerves.first).to be_instance_of Nerve
+    end
 
 end
