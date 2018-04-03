600d9429d3e8dbb8362460d51ad2dd4678751fcf
Eugene <emarkine@gmail.com>
2018-04-03 10:47:09 +0200

    2018-04-03.10:47 deploy adapt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
   # User.connection
   authenticates_with_sorcery!
-  belongs_to :currency
-  belongs_to :country
-  validates :email, email: true, presence: true, uniqueness: true
   validates :name, presence: true
+  validates :email, email: true, presence: true, uniqueness: true
   validates :country_id, presence: true
   validates :currency_id, presence: true
   validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  belongs_to :currency
+  belongs_to :country
 
   def self.test
     User.find_by_name 'test'
